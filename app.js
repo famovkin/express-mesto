26a2a83687fccc340b3caf66cec81b044f5166a0
Tolya Fam <famovkin@yandex.ru>
2022-02-05 19:26:07 +0300

    feat: создать схемы и модели user, card

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect('mongodb://localhost:27017/mydb');
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
