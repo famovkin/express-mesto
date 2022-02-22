f3724397d5e011a3596c61ad11f5c18196fc8121
Tolya Fam <famovkin@yandex.ru>
2022-02-22 05:25:24 +0300

    refactor: использовать регулярку в модели User для поля avatar

--- models/user.js
+++ models/user.js
@@ -18,8 +18,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
-      validator: (v) => validator.isURL(v),
-      message: 'Проверь правильность ввода аватара. В этом поле должна быть ссылка на картинку',
+      validator: (v) => /^(http:\/\/|https:\/\/)(www\.)?[\w\-\._~:\/?#\[\]@!$&'\(\)*\+,;=]+(#)?$/.test(v),
+      message: 'Некорректная ссылка. Проверьте правильность ссылки на аватар',
     },
   },
   email: {
