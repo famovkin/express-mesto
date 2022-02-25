a6a1069dee5c01f804b9e4f84f61b77cb0e44409
Tolya Fam <famovkin@yandex.ru>
2022-02-25 18:12:09 +0300

    fix: добавить необязательные поля при регистрации

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 const { login, createUser } = require('../controllers/users');
 const NotFoundError = require('../errors/notFoundError');
+const validateURL = require('../utils/validateURL');
 
 const routes = express.Router();
 
@@ -18,12 +19,15 @@ routes.post('/signup', express.json(), celebrate({
   body: Joi.object().keys({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
-  }).unknown(true),
+    name: Joi.string().min(2),
+    about: Joi.string().min(2),
+    avatar: Joi.string().custom(validateURL, 'URL validation'),
+  }),
 }), createUser);
 routes.use(auth);
 routes.use('/users', userRoutes);
 routes.use('/cards', cardRoutes);
-routes.use('*', (req, res) => {
+routes.use('*', () => {
   throw new NotFoundError('Страница не найдена. Проверьте URL');
 });
 
